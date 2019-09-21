9785a26ce37309d3734882eaf8388a2b45b2ebdc
Danilo Beleño <dbeleno86@uan.edu.co>
2019-09-21 18:21:11 -0500

    vista 2 agregada

--- app/src/main/java/co/edu/uan/madlibs/MainActivity.kt
+++ app/src/main/java/co/edu/uan/madlibs/MainActivity.kt
@@ -1,12 +1,20 @@
 package co.edu.uan.madlibs
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun Play(view: View){
+            val intent = Intent(this, co.edu.uan.madlibs.wordsView::class.java)
+            startActivity(intent)
     }
 }
---
+++ app/src/main/java/co/edu/uan/madlibs/wordsView.kt
@@ -0,0 +1,12 @@
+package co.edu.uan.madlibs
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class wordsView : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_words_view)
+    }
+}
